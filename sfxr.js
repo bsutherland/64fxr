3dfa60f84c8bae6a912aa5be4bb44ae7ee32ce8c
Bruce Sutherland <bsutherland@users.noreply.github.com>
2019-12-12 22:53:57 +0900

    Reasonably useable UI and implementation.

--- sfxr.js
+++ sfxr.js
@@ -29,10 +29,19 @@ SFXR = function(opts) {
 
   this.A = document.getElementById('A');
   this.D = document.getElementById('D');
+  this.S = document.getElementById('S');
+  this.R = document.getElementById('R');
+  this.PW = document.getElementById('PW');
+
   this.WAVEFORMS = [
     'noi', 'pul', 'saw', 'tri'
   ].map(id => document.getElementById(id));
-  this.PW = document.getElementById('PW');
+
+  this.OFF = document.getElementById('OFF');
+  this.LP = document.getElementById('LP');
+  this.HP = document.getElementById('HP');
+  this.FC = document.getElementById('FC');
+  this.Q = document.getElementById('Q');
 
   this.palette = new Map(['light-blue', 'blue'].map(c => [
     c, getComputedStyle(document.documentElement).getPropertyValue('--'+c)
@@ -46,10 +55,8 @@ SFXR = function(opts) {
 
 // Pico.js hook for processing
 SFXR.prototype.process = function(L, R) {
-  // console.log('process');
   if (this.ready) {
     var written = this.generateIntoBuffer(L.length, L, 0);
-    // console.log(written, 'bytes written')
     if (written === 0) {
       this.ready = false;
       this.finished = true;
@@ -75,22 +82,35 @@ SFXR.prototype.process = function(L, R) {
 };
 
 SFXR.prototype.initSID = function() {
-  this.synth.poke(0x18, 0xf); // set full volume
   this.attack();
   this.decay();
-  this.waveforms();
+  this.sustain();
+  this.release();
   this.pulsewidth();
+  this.waveforms();
+  this.filtertype();
+  this.filtercutoff();
+  this.filterres();
 }
 
 SFXR.prototype.play = function() {
-  this.ready = true;
-  pico.play(this);
+  if (!this.ready) {
+    this.ready = true;
+    pico.play(this);
+  }
 };
 
 SFXR.prototype.stop = function() {
   pico.pause();
   this.ready = false;
 };
+
+SFXR.prototype.keyup = function(key) {
+  if (this.lastPlayedKey == key) {
+    this.clearGate();
+    this.lastPlayedKey = null;
+  }
+}
 
 SFXR.prototype.keydown = function(key) {
   const KEYMAP = {
@@ -106,15 +126,28 @@ SFXR.prototype.keydown = function(key) {
     'n': 'a-0',
     'j': 'a#0',
     'm': 'b-0',
+    'q': 'c-1',
+    '2': 'c#1',
+    'w': 'd-1',
+    '3': 'd#1',
+    'e': 'e-1',
+    'r': 'f-1',
+    '5': 'f#1',
+    't': 'g-1',
+    '6': 'g#1',
+    'y': 'a-1',
+    '7': 'a#1',
   }
   const note = KEYMAP[key.toLowerCase()];
-  this.trigger(note);
+  if (note && this.lastPlayedKey != key) {
+    this.lastPlayedKey = key;
+    this.trigger(note);
+  }
 }
 
 SFXR.prototype.generateIntoBuffer = function(samples, data, dataOffset) {
   if (!this.ready) return 0;
   const generated = this.synth.generateIntoBuffer(samples, data, dataOffset);
-  // console.log(samples, generated, dataOffset);
   return generated;
 };
 
@@ -130,6 +163,44 @@ SFXR.prototype.decay = function() {
   this.synth.poke(0x05, this.ad);
 }
 
+SFXR.prototype.sustain = function() {
+  const x = this.S.value;
+  this.sr = (this.sr & 0x0f) | ((x&0xf) << 4);
+  this.synth.poke(0x06, this.sr);
+}
+
+SFXR.prototype.release = function() {
+  const x = this.R.value;
+  this.sr = (this.sr & 0xf0) | (x&0xf);
+  this.synth.poke(0x06, this.sr);
+}
+
+SFXR.prototype.pulsewidth = function() {
+  const x = this.PW.value;
+  this.synth.poke(0x02, x & 0xf);
+  this.synth.poke(0x03, x >> 8);
+}
+
+SFXR.prototype.filtertype = function() {
+  let x = 0;
+  if (this.HP.checked) x |= 0x4;
+  if (this.LP.checked) x |= 0x1;
+  this.filterres();
+  this.synth.poke(0x18, (x<<4) | 0xf); // always full volume
+}
+
+SFXR.prototype.filterres = function() {
+  const x = this.Q.value << 4;
+  const y = this.OFF.checked ? 0 : 1;
+  this.synth.poke(0x17, x | y); // make sure filter is enabled on ch1
+}
+
+SFXR.prototype.filtercutoff = function() {
+  const x = this.FC.value;
+  this.synth.poke(0x15, x & 0x7);
+  this.synth.poke(0x16, (x >> 3) & 0xff);
+}
+
 SFXR.prototype.waveforms = function() {
   let x = 0;
   this.WAVEFORMS.forEach((cb) => {
@@ -140,10 +211,9 @@ SFXR.prototype.waveforms = function() {
   this.synth.poke(0x04, this.ctrl);
 }
 
-SFXR.prototype.pulsewidth = function() {
-  const x = this.PW.value;
-  this.synth.poke(0x02, x & 0xf);
-  this.synth.poke(0x03, x >> 8);
+SFXR.prototype.clearGate = function() {
+  this.ctrl &= ~1;
+  this.synth.poke(0x04, this.ctrl);
 }
 
 SFXR.prototype.trigger = function(note) {
@@ -161,8 +231,19 @@ SFXR.prototype.trigger = function(note) {
     'a-0': 0x7476,
     'a#0': 0x7b63,
     'b-0': 0x82b9,
+    'c-1': 0x8a7f,
+    'c#1': 0x92bc,
+    'd-1': 0x9b75,
+    'd#1': 0xa4b4,
+    'e-1': 0xae7f,
+    'f-1': 0xb8df,
+    'f#1': 0xc3de,
+    'g-1': 0xcf84,
+    'g#1': 0xdbd9,
+    'a-1': 0xe8ed,
+    'a#1': 0xf6c6,
   };
-  const reg16 = NOTEMAP[note] / 16;
+  const reg16 = NOTEMAP[note] / 32;
   // because Chrome requires a click before sound can start.
   this.play();
 
@@ -171,8 +252,6 @@ SFXR.prototype.trigger = function(note) {
   this.synth.poke(0x01, reg16 >> 8);
   this.synth.poke(0x00, reg16 & 0xff);
 
-  this.ctrl &= ~1;
-  this.synth.poke(0x04, this.ctrl);
   this.ctrl |= 1;
   this.synth.poke(0x04, this.ctrl);
 }
